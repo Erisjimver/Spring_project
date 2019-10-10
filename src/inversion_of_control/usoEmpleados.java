7303678d3297249cb3b0aa8871cc760625dd6d2a
erisjimver <erisjinver@gmail.com>
2019-10-10 18:49:18 -0500

    definitivamente no funciona

--- src/inversion_of_control/usoEmpleados.java
+++ src/inversion_of_control/usoEmpleados.java
@@ -9,7 +9,7 @@ public class usoEmpleados {
     public static void main(String[] args) {
         
         
-        ClassPathXmlApplicationContext contexto=new ClassPathXmlApplicationContext("applicationContext3.xml");
+
         //Creacion de objetos de tipo empleado
        /* 
         Empleados Empleado1=new jefeEmpleado();
@@ -36,8 +36,43 @@ public class usoEmpleados {
         
         contexto.close();
        */
-       Empleados Maria=contexto.getBean("miEmpleado",Empleados.class);
-       System.out.println(Maria.getInformes());
+
+
+
+
+
+        ClassPathXmlApplicationContext contexto=new ClassPathXmlApplicationContext("applicationContext2.xml");
+        
+        //peticion de beans al contenedor Spring
+        Empleados Israel=contexto.getBean("secretarioEmpleado",Empleados.class);
+        Empleados Jimena=contexto.getBean("secretarioEmpleado",Empleados.class);
+        Empleados Manolo=contexto.getBean("secretarioEmpleado",Empleados.class);
+        Empleados Juanco=contexto.getBean("secretarioEmpleado",Empleados.class);
+        Empleados Solang=contexto.getBean("secretarioEmpleado",Empleados.class);
+        Empleados Maria=contexto.getBean("secretarioEmpleado",Empleados.class);
+        System.out.println(Israel);
+        System.out.println(Jimena);
+        System.out.println(Manolo);
+        System.out.println(Juanco);
+        System.out.println(Solang);
+        
+        
+        
+        System.out.println("Tareas del director: "+Maria.getTareas());
+        System.out.println("Informes del director: "+Maria.getInformes());
+        System.out.println("El correo es: "+Maria.getEmail());
+        System.out.println("El nombre de la empresa es: "+Maria.getNombreEmpresa());
+        
+        
+        if(Israel==Jimena){
+            System.out.println("Apuntan al mismo objeto");
+        }
+        else{
+            System.out.println("No se trata del mismo objeto");
+        }
+        contexto.close();
+
+
         
     }
     
