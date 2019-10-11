80e8009465e2c42cd53510da63d5619ab961cd8e
erisjimver <erisjinver@gmail.com>
2019-10-11 18:36:59 -0500

    correcion de cieertas cosas sigue sin funcionar secretario 2

--- src/inversion_of_control/usoEmpleados.java
+++ src/inversion_of_control/usoEmpleados.java
@@ -41,35 +41,21 @@ public class usoEmpleados {
 
 
 
-        ClassPathXmlApplicationContext contexto=new ClassPathXmlApplicationContext("applicationContext2.xml");
+        ClassPathXmlApplicationContext contexto=new ClassPathXmlApplicationContext("applicationContext3.xml");
         
-        //peticion de beans al contenedor Spring
-        Empleados Israel=contexto.getBean("secretarioEmpleado",Empleados.class);
-        Empleados Jimena=contexto.getBean("secretarioEmpleado",Empleados.class);
-        Empleados Manolo=contexto.getBean("secretarioEmpleado",Empleados.class);
-        Empleados Juanco=contexto.getBean("secretarioEmpleado",Empleados.class);
-        Empleados Solang=contexto.getBean("secretarioEmpleado",Empleados.class);
-        Empleados Maria=contexto.getBean("secretarioEmpleado",Empleados.class);
-        System.out.println(Israel);
-        System.out.println(Jimena);
-        System.out.println(Manolo);
-        System.out.println(Juanco);
-        System.out.println(Solang);
+     
+
+        Empleados Maria=contexto.getBean("secretarioEmpleado2",Empleados.class);
+
         
         
         
-        System.out.println("Tareas del director: "+Maria.getTareas());
+        //System.out.println("Tareas del director: "+Maria.getTareas());
         System.out.println("Informes del director: "+Maria.getInformes());
-        System.out.println("El correo es: "+Maria.getEmail());
-        System.out.println("El nombre de la empresa es: "+Maria.getNombreEmpresa());
+        //System.out.println("El correo es: "+Maria.getEmail());
+        //System.out.println("El nombre de la empresa es: "+Maria.getNombreEmpresa());
         
-        
-        if(Israel==Jimena){
-            System.out.println("Apuntan al mismo objeto");
-        }
-        else{
-            System.out.println("No se trata del mismo objeto");
-        }
+
         contexto.close();
 
 
